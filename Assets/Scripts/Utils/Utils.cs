bb5ae2adf01d192424c37cb5bcdd8ed73b6976d7
SergioCMDev <sergiocmdev@gmail.com>
2022-01-14 20:37:43 +0100

    actualiacion utils

--- Assets/Scripts/Utils/Utils.cs
+++ Assets/Scripts/Utils/Utils.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -54,6 +56,35 @@ namespace UtilUnity
         public static float RoundValue(float value, int decimals)
         {
             return (float) Decimal.Round((decimal) value, decimals);
+        }
+        
+        public static int GetRandomValue(int min, int max)
+        {
+            var randomValue = Random.Range(min, max);
+
+            return randomValue;
+        }
+
+        public static Transform GetRandomPosition(List<Transform> positions)
+        {
+            if (positions.Count <= 0) return null;
+            var randomPosition = GetRandomValue(0, positions.Count - 1);
+            return positions[randomPosition];
+        }
+
+        public static bool IsLayerInLayerMask(LayerMask layerMask, int layer)
+        {
+            return layerMask == (layerMask | (1 << layer));
+        }
+
+        public static int GetMaximumValueInList(List<int> valuesToCheck)
+        {
+            return valuesToCheck.Prepend(Int32.MinValue).Max();
+        }
+    
+        public static int GetMinimumValueInList(List<int> valuesToCheck)
+        {
+            return valuesToCheck.Prepend(Int32.MaxValue).Min();
         }
     }
 }
