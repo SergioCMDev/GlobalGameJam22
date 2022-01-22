d31584c6f5d0b97c48598277e11d558487030ee4
Sergio CM <sergiocmdev@gmail.com>
2022-01-22 21:32:14 +0100

    Cambios varios

--- Assets/ResourcesManager.cs
+++ Assets/ResourcesManager.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 
 public class ResourcesManager : MonoBehaviour
 {
+    //TODO Resource Model to store quantities of each resource
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,5 +15,6 @@ public class ResourcesManager : MonoBehaviour
     public void PlayerGetResource(PlayerGetResourceEvent playerGetResourceEvent)
     {
         Debug.Log($"Got resource {playerGetResourceEvent.Type} Q {playerGetResourceEvent.Quantity}");
+        //IF player Gets resource check if any building can be updated based on a scriptable object with level-resource quantities and type of upgrade
     }
 }
--- Assets/UtilsMouse.cs
+++ Assets/UtilsMouse.cs
@@ -6,4 +6,9 @@ public static class UtilsMouse
     {
         return Mouse.current.leftButton.wasPressedThisFrame;
     }
+    
+    public static bool IsRightButtonPressedThisFrame()
+    {
+        return Mouse.current.rightButton.wasPressedThisFrame;
+    }
 }
