15b14dc8dfcdeb8d00aa3a408159b889b1dadd92
Adolfo Ingelmo <Lodur_norbel@hotmail.com>
2022-01-29 19:18:13 +0100

    Finished pathfinding

--- Assets/Scripts/IA/GridPathfinding.cs
+++ Assets/Scripts/IA/GridPathfinding.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.Serialization;
 using UnityEngine.Tilemaps;
 
@@ -9,59 +10,89 @@ namespace IA
 {
     public class GridPathfinding:MonoBehaviour
     {
-        [SerializeField] 
-        private Tilemap _tilemap;
-
-        [SerializeField]
-        private Enemy _enemy;
+        [SerializeField] private Tilemap _tilemap;
+        [SerializeField] private Enemy _enemy;
+        [SerializeField] private bool friend;
 
         [FormerlySerializedAs("availablePlaces")] public List<Vector3> yellowBrickRoad;
         public List<Vector3> world;
+        public List<Vector3> defensiveBuilds;
+
         public Vector3 nextDestination;
+        public Vector3 lastBrick;
+        public bool attacking = false;
+
+        public Vector3 start;
+        public Vector3 end;
+        
         
         private void Awake()
         {
-            _enemy.InitialPosition(_tilemap.cellBounds.max);
+            start = (friend) ? _tilemap.cellBounds.min : _tilemap.cellBounds.max;
+            end = (friend) ? _tilemap.cellBounds.max : _tilemap.cellBounds.min;
+            _enemy.InitialPosition(start);
             GetWorld();
+            nextDestination = Pathfinder(_enemy.transform.position);
         }
 
         private void Update()
         {
-            _enemy.MoveTo(nextDestination);
             if (_enemy.transform.position == nextDestination)
             {
-                nextDestination = Pathfinder(_enemy.transform.position);
+                if (attacking)
+                {
+                    attacking = false;
+                    nextDestination = lastBrick;
+                }
+                else
+                {
+                    nextDestination = Pathfinder(_enemy.transform.position);
+                }
             }
+            _enemy.MoveTo(nextDestination);
         }
 
         private Vector3 Pathfinder(Vector3 currentPos)
         {
-            Vector3 aux = new Vector3();
+            Vector3 bestOptionVector = new Vector3();
             float bestOption = 99999;
-            float HValue = 0;
-
+            
+            //activar si se quiere atacar a los edificios
+            //if (!yellowBrickRoad.Any() && !defensiveBuilds.Any())
             if (!yellowBrickRoad.Any())
             {
-                aux =_tilemap.cellBounds.min;
+                bestOptionVector = end;
             }
-            
-            foreach(var brick in yellowBrickRoad)
+            else
             {
-                HValue = HeuristicValue(currentPos, brick);
-                if (HValue < bestOption)
-                {
-                    aux = brick;
-                    bestOption = HValue;
-                };
+                HeuristicValue(currentPos, yellowBrickRoad, ref bestOption,  ref bestOptionVector);
+                //activar si se quiere atacar a los edificios
+                //HeuristicValue(currentPos, defensiveBuilds, ref bestOption, ref bestOptionVector);
             }
-            yellowBrickRoad.Remove(aux);
 
-            return aux;
+            if (defensiveBuilds.Contains(bestOptionVector))
+            {
+                defensiveBuilds.Remove(bestOptionVector);
+                attacking = true;
+                lastBrick = nextDestination;
+            }
+            else if(yellowBrickRoad.Contains(bestOptionVector))
+            {
+                yellowBrickRoad.Remove(bestOptionVector);
+                attacking = false;
+            }else if (bestOptionVector == end)
+            {
+                defensiveBuilds.Clear();
+                yellowBrickRoad.Clear();
+            }
+
+            return bestOptionVector;
         }
 
         public void GetWorld()
         {
             yellowBrickRoad = new List<Vector3>();
+            defensiveBuilds = new List<Vector3>();
             for (int n = _tilemap.cellBounds.xMin; n < _tilemap.cellBounds.xMax; n++)
             {
                 for (int p = _tilemap.cellBounds.yMin; p < _tilemap.cellBounds.yMax; p++)
@@ -71,18 +102,35 @@ namespace IA
                     if (_tilemap.HasTile(localPlace))
                     {
                         world.Add(place);
-                        if (!_tilemap.GetTile(localPlace).name.Equals("tileGrass02"))
+                        if (_tilemap.GetTile(localPlace).name.Equals("tileStones02"))
                         {
                             yellowBrickRoad.Add(place);
+                        } 
+                        else if (_tilemap.GetTile(localPlace).name.Equals("bricks02"))
+                        {
+                            defensiveBuilds.Add(place);
                         }
                     }
                 }
             }
         }
 
-        private float HeuristicValue(Vector3 current, Vector3 candidate)
+        private void HeuristicValue(Vector3 current, List<Vector3> ListCandidates, ref float result, ref Vector3 chosen)
         {
-            return Vector3.Distance(current, candidate);
+            float HValue;
+            
+            chosen = end;
+            result = Math.Abs(current.x - end.x) + Math.Abs(current.y - end.y)-10;
+            foreach(var tile in ListCandidates)
+            {
+                HValue = Math.Abs(current.x - tile.x) + Math.Abs(current.y - tile.y);
+                if (HValue < result)
+                {
+                    chosen = tile;
+                    result = HValue;
+                };
+            }
         }
+        
     }
 }
