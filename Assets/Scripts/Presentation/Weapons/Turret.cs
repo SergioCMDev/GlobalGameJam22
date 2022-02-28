6d14670cbe13fd777ec3c32dcff8a9da7cd261a5
Sergio CM <sergiocmdev@gmail.com>
2022-02-28 14:30:21 +0100

    Refractor CanReachTurret

--- Assets/Scripts/Presentation/Weapons/Turret.cs
+++ Assets/Scripts/Presentation/Weapons/Turret.cs
@@ -13,24 +13,31 @@ namespace Presentation.Weapons
             // throw new NotImplementedException();
         }
 
+        
+        // (x1-x2)^2 + (y1-y2)^2 <= range * range
 
         protected override bool CanReach(GameObject objectToAttack)
         {
+            //Method 1
             // var distance = objectToAttack.transform.position - transform.position;
             // var magninute = distance.magnitude / 2;
             // return magninute < 2.5;
 
-            var xDif = transform.position.x - objectToAttack.transform.position.x;
-            var yDif = transform.position.y - objectToAttack.transform.position.y;
-            var distance = yDif / 2 + xDif;
-            var roundToInt = Mathf.RoundToInt(distance);
-            Debug.Log($"DIstance {distance} xDif {xDif} ydif {yDif}");
-            return roundToInt < AttackRingRange;
-            // return Mathf.Pow(transform.position.x - transform.position.y, 2) +
-            //        Mathf.Pow(objectToAttack.transform.position.x - objectToAttack.transform.position.y, 2) <=
-            //        AttackRingRange * AttackRingRange;
-            // return VectorUtils.VectorIsNearVector(transform.position, objectToAttack.transform.position,
-            //     AttackRingRange * _offsetForEachTile);
+            // var xDif = transform.position.x - objectToAttack.transform.position.x;
+            // var yDif = transform.position.y - objectToAttack.transform.position.y;
+            // var distance = yDif / 2 + xDif;
+            // var roundToInt = Mathf.RoundToInt(distance);
+            // Debug.Log($"DIstance {distance} xDif {xDif} ydif {yDif}");
+            // return roundToInt < AttackRingRange;
+            
+            //Method2
+            return Mathf.Pow(transform.position.x - objectToAttack.transform.position.x, 2) +
+                   Mathf.Pow(transform.position.y - objectToAttack.transform.position.y, 2) <=
+                   AttackRingRange * AttackRingRange;
+            
+            //Method3
+            return VectorUtils.VectorIsNearVector(transform.position, objectToAttack.transform.position,
+                AttackRingRange * _offsetForEachTile);
         }
     }
 }
