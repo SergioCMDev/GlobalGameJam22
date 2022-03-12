f3ef9a06603f5808fcdbd9a4f5c4e8067371d7be
SergioCMDev <sergiocmdev@gmail.com>
2022-03-12 14:00:48 +0100

    Mejoras torreta para hacer animaciones de ataque y de desplegado

--- Assets/Scripts/Presentation/BuildingManager.cs
+++ Assets/Scripts/Presentation/BuildingManager.cs
@@ -69,6 +69,7 @@ namespace Presentation
         {
             var militaryBuilding = buildingEvent.Instance.GetComponent<MilitaryBuildingFacade>();
             militaryBuilding.SetEnemyToAttack(_enemy);
+            militaryBuilding.Deploy();
             _ownMilitaryBuilding.Add(militaryBuilding);
         }
 
