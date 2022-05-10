2f7d60f7661c363db618c79cf193c7be203de398
Sergio CM <sergiocmdev@gmail.com>
2022-05-10 23:40:58 +0200

    Añadido nuevo scriptable para pruebas de pathfinding

---
+++ Assets/Scripts/Presentation/Infrastructure/CityInfo.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Presentation.Infrastructure
+{
+    public class CityInfo : ScriptableObject
+    {
+        public int MaxLife;
+        public int CitySize;
+    }
+}
