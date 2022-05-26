a5622b13f1b6f308100a3d750fedf4a088b58218
Sergio CM <sergiocmdev@gmail.com>
2022-05-26 20:06:24 +0200

    refactor installers

--- Assets/Scripts/Installers/GameInstaller.cs
+++ Assets/Scripts/Installers/GameInstaller.cs
@@ -11,7 +11,7 @@ using Utils;
 
 namespace Installers
 {
-    public class Installer : MonoBehaviour
+    public class GameInstaller : MonoBehaviour
     {
         [SerializeField] private GameObject _readInputPlayerPrefab;
         [SerializeField] private GameObject _sceneChangerPrefab;
@@ -21,7 +21,6 @@ namespace Installers
         [SerializeField] private GameObject _popupManagerPrefab;
 
         [SerializeField] private GameObject _resourcesManagerPrefab;
-        // [SerializeField] private GameObject _collectibleManagerPrefab;
 
         private GameObject _readInputPlayerInstance,
             _sceneChangerInstance,
@@ -37,49 +36,78 @@ namespace Installers
 
         private void Awake()
         {
-            var installersObject = FindObjectsOfType<Installer>(true);
+            var installersObject = FindObjectsOfType<GameInstaller>(true);
             if (installersObject.Any(x => x.Initialized)) return;
 
+            var initInstallersObject = FindObjectsOfType<InitInstaller>(true);
+            if (initInstallersObject.Any(x => x.Initialized))
+            {
+                InitGameLogic();
+            }
+            else
+            {
+                InitCommonLogic();
+                InitGameLogic();
+            }
+
+            DontDestroyOnLoad(this);
+
+            _initialized = true;
+        }
+
+        private void InitCommonLogic()
+        {
             _readInputPlayerInstance = Instantiate(_readInputPlayerPrefab);
             _sceneChangerInstance = Instantiate(_sceneChangerPrefab);
             _soundManagerInstance = Instantiate(_soundManagerPrefab);
             _timeManagerInstance = Instantiate(_timeManagerPrefab);
+            _popupManagerInstance = Instantiate(_popupManagerPrefab);
+
             _buildingManagerInstance = Instantiate(_buildingManagerPrefab);
             _resourcesManagerInstance = Instantiate(_resourcesManagerPrefab);
-            _popupManagerInstance = Instantiate(_popupManagerPrefab);
-            
+            ServiceLocator.Instance.RegisterService<SoundManager>(
+                _soundManagerInstance.GetComponent<SoundManager>());
+            ServiceLocator.Instance.RegisterService<ReadInputPlayer>(
+                _readInputPlayerInstance.GetComponent<ReadInputPlayer>());
+            ServiceLocator.Instance.RegisterService<SceneChanger>(
+                _sceneChangerInstance.GetComponent<SceneChanger>());
+            ServiceLocator.Instance.RegisterService<PopupManager>(
+                _popupManagerInstance.GetComponent<PopupManager>());
+            ServiceLocator.Instance.RegisterService<TimeManager>(_timeManagerInstance.GetComponent<TimeManager>());
+
+
             //TODO HACERLO PARA N OBJETOS NO SOLO 1
             ServiceLocator.Instance.RegisterService<IJsonator>(new JsonUtililyTransformer());
             ServiceLocator.Instance.RegisterService<ISaver>(new SaveUsingPlayerPrefs());
             ServiceLocator.Instance.RegisterService<ILoader>(new LoadWithPlayerPrefs());
             ServiceLocator.Instance.RegisterService<GameDataService>(new GameDataService());
             ServiceLocator.Instance.RegisterService<SoundDataService>(new SoundDataService());
-
-            ServiceLocator.Instance.RegisterService<SoundManager>(_soundManagerInstance.GetComponent<SoundManager>());
-            ServiceLocator.Instance.RegisterService<ReadInputPlayer>(
-                _readInputPlayerInstance.GetComponent<ReadInputPlayer>());
-            ServiceLocator.Instance.RegisterService<SceneChanger>(_sceneChangerInstance.GetComponent<SceneChanger>());
-            ServiceLocator.Instance.RegisterService<PopupManager>(_popupManagerInstance.GetComponent<PopupManager>());
-            ServiceLocator.Instance.RegisterService<TimeManager>(_timeManagerInstance.GetComponent<TimeManager>());
-            ServiceLocator.Instance.RegisterService<ResourcesManager>(_resourcesManagerInstance.GetComponent<ResourcesManager>());
-            ServiceLocator.Instance.RegisterService<BuildingManager>(_buildingManagerInstance
-                .GetComponent<BuildingManager>());
-
+            
             ServiceLocator.Instance.RegisterModel<IPlayerModel>(new PlayerModel());
             ServiceLocator.Instance.RegisterModel<ISceneModel>(new SceneModel());
-            ServiceLocator.Instance.RegisterModel<IResourcesModel>(new ResourcesModel());
-            ServiceLocator.Instance.RegisterModel<IBuildingStatusModel>(new BuildingStatusModel());
 
-            // DontDestroyOnLoad(this);
-            DontDestroyOnLoad(this);
             DontDestroyOnLoad(_sceneChangerInstance);
             DontDestroyOnLoad(_readInputPlayerInstance);
             DontDestroyOnLoad(_soundManagerInstance);
             DontDestroyOnLoad(_timeManagerInstance);
+            DontDestroyOnLoad(_popupManagerInstance);
+        }
+
+        private void InitGameLogic()
+        {
+            _buildingManagerInstance = Instantiate(_buildingManagerPrefab);
+            _resourcesManagerInstance = Instantiate(_resourcesManagerPrefab);
+            ServiceLocator.Instance.RegisterService<ResourcesManager>(_resourcesManagerInstance
+                .GetComponent<ResourcesManager>());
+            ServiceLocator.Instance.RegisterService<BuildingManager>(_buildingManagerInstance
+                .GetComponent<BuildingManager>());
+
+
+            ServiceLocator.Instance.RegisterModel<IResourcesModel>(new ResourcesModel());
+            ServiceLocator.Instance.RegisterModel<IBuildingStatusModel>(new BuildingStatusModel());
+
             DontDestroyOnLoad(_resourcesManagerInstance);
             DontDestroyOnLoad(_buildingManagerInstance);
-            DontDestroyOnLoad(_popupManagerInstance);
-            _initialized = true;
         }
 
         private void OnDestroy()
