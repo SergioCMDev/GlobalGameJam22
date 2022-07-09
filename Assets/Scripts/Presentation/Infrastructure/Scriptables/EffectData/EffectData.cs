dc126f46f924a6f54503283a8167094375818d19
Sergio CM <sergiocmdev@gmail.com>
2022-07-09 12:33:34 +0200

    Renamed y moved archivos

--- Assets/Scripts/Presentation/Infrastructure/Scriptables/EffectData/EffectData.cs
+++ Assets/Scripts/Presentation/Infrastructure/Scriptables/EffectData/EffectData.cs
@@ -3,8 +3,7 @@ using UnityEngine;
 
 namespace Presentation.Infrastructure.Scriptables
 {
-    [CreateAssetMenu(fileName = "AttackBehaviourData", menuName = "Turrets/AttackBehaviourData/Turrets")]
-    public class EffectData : ScriptableObject
+    public abstract class EffectData : ScriptableObject
     {
         public DamageType damageType;
         public float cadence, damage;
