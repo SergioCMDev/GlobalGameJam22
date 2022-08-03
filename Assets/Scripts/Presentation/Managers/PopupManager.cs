10dd372e07b59d6473b042e58a69b229bedec491
Sergio CM <sergiocmdev@gmail.com>
2022-08-03 20:12:24 +0200

    refactor Popupmanager

--- Assets/Scripts/Presentation/Managers/PopupManager.cs
+++ Assets/Scripts/Presentation/Managers/PopupManager.cs
@@ -25,7 +25,7 @@ namespace Presentation.Managers
     public class PopupManager : MonoBehaviour
     {
         [SerializeField] private PopupList _popupList;
-        private GameObject _currentopenedPopup;
+        private GameObject _currentOpenedPopup;
         private Camera _camera;
 
         private void Start()
@@ -40,19 +40,19 @@ namespace Presentation.Managers
 
         public T InstantiatePopup<T>(PopupType popupType)
         {
-            _currentopenedPopup = InstantiatePopup(popupType);
-            return _currentopenedPopup.GetComponent<T>();
+            _currentOpenedPopup = InstantiatePopup(popupType);
+            return _currentOpenedPopup.GetComponent<T>();
         }
 
         private GameObject InstantiatePopup(PopupType popupType)
         {
             GameObject prefab = _popupList.GetPrefabByType(popupType);
-            _currentopenedPopup = Instantiate(prefab, transform, false);
-            var closeablePopup = _currentopenedPopup.GetComponent<ICloseablePopup>();
+            _currentOpenedPopup = Instantiate(prefab, transform, false);
+            var closeablePopup = _currentOpenedPopup.GetComponent<ICloseablePopup>();
             closeablePopup.HasToClosePopup += ClosePopup;
-            _currentopenedPopup.gameObject.SetActive(false);
-            _currentopenedPopup.GetComponentInChildren<Canvas>().worldCamera = _camera;
-            return _currentopenedPopup;
+            _currentOpenedPopup.gameObject.SetActive(false);
+            _currentOpenedPopup.GetComponentInChildren<Canvas>().worldCamera = _camera;
+            return _currentOpenedPopup;
         }
 
         private void ClosePopup(GameObject popup)
@@ -60,6 +60,10 @@ namespace Presentation.Managers
             popup.SetActive(false);
             var closeablePopup = popup.GetComponent<ICloseablePopup>();
             closeablePopup.PopupHasBeenClosed.Invoke();
+        }
+        public void CloseCurrentOpenedPopup()
+        {
+            ClosePopup(_currentOpenedPopup);
         }
     }
 }
